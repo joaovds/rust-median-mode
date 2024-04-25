8755c012b9222c319758359d7a1b1f0108f296ab
João Victor da Silva <jv782063@gmail.com>
2024-04-25 10:49:15 -0300

    find median

--- src/main.rs
+++ src/main.rs
@@ -23,5 +23,21 @@ fn main() {
 
     input_numbers.sort();
 
-    println!("{:?}", input_numbers)
+    let median = find_the_median(&input_numbers);
+
+    println!("{:?}", input_numbers);
+    println!("{:?}", median);
+}
+
+fn find_the_median(numbers: &Vec<u32>) -> u32 {
+    let center_index = numbers.len() / 2;
+
+    if numbers.len() % 2 == 0 {
+        let center_left = numbers[center_index - 1];
+        let center_right = numbers[center_index];
+
+        return (center_left + center_right) / 2;
+    }
+
+    return numbers[center_index];
 }
