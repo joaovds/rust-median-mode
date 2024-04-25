e0b6cc33d40de7ec7130309565cf92d57bfaeee9
João Victor da Silva <jv782063@gmail.com>
2024-04-25 10:25:36 -0300

    data input by the user, and conversion to integer forming a vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the integers one after the other:");
+
+    let mut input_numbers = String::new();
+
+    io::stdin()
+        .read_line(&mut input_numbers)
+        .expect("Failed to read line!");
+
+    let input_numbers: Vec<u32> = input_numbers
+        .trim()
+        .split_whitespace()
+        .filter_map(|number| match number.trim().parse() {
+            Ok(n) => Some(n),
+            Err(_) => {
+                println!("'{}' is invalid number!", number);
+                None
+            }
+        })
+        .collect();
+
+    println!("{:?}", input_numbers)
 }
